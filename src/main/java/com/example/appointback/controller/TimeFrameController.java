72125fb7eb16a9689dae9356494f78e6de266455
MDi <calendarzyk@gmail.com>
2022-06-20 15:46:24 +0200

    timeframe generation

--- src/main/java/com/example/appointback/controller/TimeFrameController.java
+++ src/main/java/com/example/appointback/controller/TimeFrameController.java
@@ -1,9 +1,13 @@
 package com.example.appointback.controller;
 
+import com.example.appointback.entity.Doctor;
+import com.example.appointback.entity.TimeFrame;
 import com.example.appointback.entity.TimeFrameDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -13,6 +17,7 @@ public class TimeFrameController {
 
     private final TimeFrameMapper mapper;
     private final TimeFrameRepository repository;
+    private final DoctorRepository docRepository;
 
     @GetMapping("/{timeFrameId}")
     public TimeFrameDto getTimeFrame(@PathVariable Long timeFrameId) {
@@ -37,5 +42,32 @@ public class TimeFrameController {
     @DeleteMapping("/{timeFrameId}")
     public void deleteTimeFrame(@PathVariable Long timeFrameId) {
         repository.deleteById(timeFrameId);
+    }
+
+    public List<TimeFrameDto> autoCreateTimeFrames() {
+        LocalDate today = LocalDate.now();
+
+        List<Doctor> doctorList = docRepository.findAll();
+        for(Doctor doc : doctorList) {
+            List<TimeFrame> tfList = doc.getTimeFrames();
+            TimeFrame current = null;
+            for(TimeFrame singleTF : tfList) {
+                if(today.equals(singleTF.getTimeframeDate())) {
+                    current = singleTF;
+                }
+            }
+            int todaysIndex = tfList.indexOf(current);
+            for (int n = 1; n < 32; n++) {
+                // TimeFrame singleTF = tfList.get(i);
+                TimeFrame next = tfList.get(todaysIndex);
+
+
+                assert current != null;
+                if(today.plusDays(n).equals(next)) {
+                    //create tf for that day.
+                }
+            }
+        }
+        return null;
     }
 }
