1ac8fb9eaf9ec5e3c7bb8464904fb171c705f063
MDi <calendarzyk@gmail.com>
2023-03-03 18:42:25 +0100

    quick save

--- src/main/java/com/example/appointback/controller/DoctorRepository.java
+++ src/main/java/com/example/appointback/controller/DoctorRepository.java
@@ -1,13 +1,12 @@
 package com.example.appointback.controller;
 
-import com.example.appointback.entity.CalendarHolder;
 import com.example.appointback.entity.Doctor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface DoctorRepository extends CrudRepository<CalendarHolder, Long> {
+public interface DoctorRepository extends CrudRepository<Doctor, Long> {
 
-    List<CalendarHolder> findAll();
+    List<Doctor> findAll();
 }
