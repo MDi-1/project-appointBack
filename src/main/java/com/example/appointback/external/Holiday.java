40e855d6847a4ce193a15ae043300a84ffc033bc
Marcin <calendarzyk@gmail.com>
2023-03-17 14:17:28 +0100

    holiday api fetching development

--- src/main/java/com/example/appointback/external/Holiday.java
+++ src/main/java/com/example/appointback/external/Holiday.java
@@ -1,4 +1,16 @@
 package com.example.appointback.external;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.time.LocalDate;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Holiday {
+
+    @JsonProperty
+    private String name;
+
+    @JsonProperty
+    private LocalDate date;
 }
